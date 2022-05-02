def793ea873fed6020c0d6397d72b78ebe9c4090
Hurtado Rodriguez <j.hurtado.rodriguez@accenture.com>
2022-05-02 15:01:19 -0500

    first commit

---
+++ Controllers/DepartmentController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyCompany_BackEnd.Models;
+
+namespace MyCompany_BackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<ActionResult<Department>> Get()
+        {
+            var dep = new Department { Active= false, DepartmentName = "Dev", PK_Department = 1};
+            return Ok(dep);
+        }
+    }
+}
